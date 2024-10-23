2e92740d75713aa08970cf9b12993a57512d78de
kurosh87 <kampfer87@protonmail.com>
2024-10-23 11:43:47 -0700

    Update next.config.js to disable caching during builds

--- next.config.js
+++ next.config.js
@@ -12,12 +12,9 @@ const nextConfig = {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
   },
-  // Add this section
-  onDemandEntries: {
-    // period (in ms) where the server will keep pages in the buffer
-    maxInactiveAge: 25 * 1000,
-    // number of pages that should be kept simultaneously without being disposed
-    pagesBufferLength: 2
+  // Disable caching during builds
+  generateBuildId: async () => {
+    return `build-${new Date().getTime()}`;
   }
 };
 
